a008a3775da43628f0a813e12640d6c068ca4778
Renato Mascarenhas <renato.mascosta@gmail.com>
2012-06-15 10:30:51 -0300

    Add Responders support.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'rails', '3.2.6'
 
 gem 'pg'
 
+gem 'devise', '~> 2.0'
+gem 'responders'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
+require 'fresk_responder'
+
 class ApplicationController < ActionController::Base
+  self.responder = FreskResponder
+  respond_to :html
+
   protect_from_forgery
 end
