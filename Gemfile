f05b49859143fc0a1f7a76d1d50620d9e1da0d7b
Renato Mascarenhas <renato.mascosta@gmail.com>
2012-06-14 23:01:48 -0300

    First commit.

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source :rubygems
+
+gem 'rails', '3.2.6'
+
+gem 'pg'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'rspec-rails', '~> 2.0'
+  gem 'capybara', '~> 1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fresk::Application.config.secret_token = '5025890b24877e76ec35ee4b0cae835e52e36810c83a38519642c5b5f4166b84f149d43a8c0a14238b40d3aee4c3c7fa394fe158bf0c8e83f410da048756184c'
